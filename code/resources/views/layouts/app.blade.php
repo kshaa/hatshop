a804579ce6be27036eca794f3447aececa9fc2ab
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2019-06-06 16:50:21 +0300

    Initial frontend structure mock-up

--- code/resources/views/layouts/app.blade.php
+++ code/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="route" id="route-{{ Route::currentRouteName() }}" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,60 +15,60 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/opw_theme.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body id="app-body">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top ">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} {{ Auth::user()->surname }} <span class="caret"></span>
-                                </a>
+                        @endif
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('trade_index') }}">{{ __('Trades') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('hat_index') }}">{{ __('Hats') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('charm_index') }}">{{ __('Charms') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
             </div>
         </nav>
 
--- code/routes/web.php
+++ code/routes/web.php
@@ -13,8 +13,34 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome')->middleware('guest');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+# Every model almost (browsers don't do PUT & DELETE) follows the 7 RESTful routes
+Route::get('/trade', 'TradeController@index')->name('trade_index');
+Route::get('/trade/new', 'TradeController@new')->name('trade_new');
+Route::get('/trade/create', 'TradeController@create')->name('trade_create');
+Route::get('/trade/:id', 'TradeController@show')->name('trade_show');
+Route::get('/trade/:id/edit', 'TradeController@edit')->name('trades_edit');
+Route::get('/trade/:id/update', 'TradeController@update')->name('trade_update');
+Route::get('/trade/:id/delete', 'TradeController@delete')->name('trade_delete');
+
+Route::get('/hat', 'HatController@index')->name('hat_index');
+Route::get('/hat/new', 'HatController@new')->name('hat_new');
+Route::get('/hat/create', 'HatController@create')->name('hat_create');
+Route::get('/hat/:id', 'HatController@show')->name('hat_show');
+Route::get('/hat/:id/edit', 'HatController@edit')->name('hats_edit');
+Route::get('/hat/:id/update', 'HatController@update')->name('hat_update');
+Route::get('/hat/:id/delete', 'HatController@delete')->name('hat_delete');
+
+Route::get('/charms', 'CharmController@index')->name('charm_index');
+Route::get('/charm/new', 'CharmController@new')->name('charm_new');
+Route::get('/charm/create', 'CharmController@create')->name('charm_create');
+Route::get('/charm/:id', 'CharmController@show')->name('charm_show');
+Route::get('/charm/:id/edit', 'CharmController@edit')->name('charms_edit');
+Route::get('/charm/:id/update', 'CharmController@update')->name('charm_update');
+Route::get('/charm/:id/delete', 'CharmController@delete')->name('charm_delete');
+
